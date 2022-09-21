9e8c19a9cd1379e9ad7c9964a1ac2d9fe51ebfa4
phlgr <philipp@gartz.dev>
2022-09-21 19:12:48 +0200

    🔥 Remove starter code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "next/app";
-import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
 import { useColorScheme } from "@mantine/hooks";
 
@@ -7,24 +6,14 @@ export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   const preferredColorScheme = useColorScheme();
   return (
-    <>
-      <Head>
-        <title>Page title</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme: preferredColorScheme,
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: preferredColorScheme,
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
   );
 }
