3b4bf94d4a71380a0a94de6b9223e903cc534866
phlgr <philipp@gartz.dev>
2022-09-23 13:11:47 +0200

    ✨ Update head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,31 @@
 import { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { useColorScheme } from "@mantine/hooks";
+import Head from "next/head";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   const preferredColorScheme = useColorScheme();
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: preferredColorScheme,
-      }}
-    >
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      <Head>
+        <title>Dead Pixel Test</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <meta name="theme-color" content="#000" />
+        <meta name="description" content="Dead Pixel Test" />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: preferredColorScheme,
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   );
 }
