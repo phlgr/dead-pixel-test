4b5590d2455c35bcc03e346172b2d3591cf21dc4
phlgr <philipp@gartz.dev>
2022-09-23 13:43:01 +0200

    ✨ Add plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,30 +2,39 @@ import { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { useColorScheme } from "@mantine/hooks";
 import Head from "next/head";
+import PlausibleProvider from "next-plausible";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   const preferredColorScheme = useColorScheme();
   return (
     <>
-      <Head>
-        <title>Dead Pixel Test</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
-        <meta name="theme-color" content="#000" />
-        <meta name="description" content="Dead Pixel Test" />
-      </Head>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme: preferredColorScheme,
-        }}
+      <PlausibleProvider
+        domain="deadpixelcheck.net"
+        customDomain="https://apps.gartz.dev"
+        trackLocalhost
+        selfHosted
+        enabled
       >
-        <Component {...pageProps} />
-      </MantineProvider>
+        <Head>
+          <title>Dead Pixel Test</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          />
+          <meta name="theme-color" content="#000" />
+          <meta name="description" content="Dead Pixel Test" />
+        </Head>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme: preferredColorScheme,
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </PlausibleProvider>
     </>
   );
 }
